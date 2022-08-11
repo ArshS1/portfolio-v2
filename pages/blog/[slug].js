4e2b36eb8d02e50b60257a4678eb6596e86c4b65
ArshS1 <90710790+ArshS1@users.noreply.github.com>
2022-08-10 23:06:12 -0400

    fix overlay

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -80,50 +80,7 @@ const Post = ({ blog }) => {
           </div>
         </div>
       </div>
-      <div className="hidden md:flex fixed bottom-0 left-[50%] translate-x-[-50%] mb-1 md:mb-5">
-        <Link href="/blogs">
-          <div className="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-[#f76c6c] transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 group mt-5">
-            <>
-              <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-[#f76c6c] group-hover:h-full"></span>
-              <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
-                <svg
-                  className="w-5 h-5 text-[#f76c6c]"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M14 5l7 7m0 0l-7 7m7-7H3"
-                  ></path>
-                </svg>
-              </span>
-              <span className="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
-                <svg
-                  className="w-5 h-5 text-white"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M14 5l7 7m0 0l-7 7m7-7H3"
-                  ></path>
-                </svg>
-              </span>
-              <span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white tracking-widest uppercase">
-                Other blogs
-              </span>
-            </>
-          </div>
-        </Link>
-      </div>
+
 
       <script
         dangerouslySetInnerHTML={{
